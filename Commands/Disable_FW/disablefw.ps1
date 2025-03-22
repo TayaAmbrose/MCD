1057348442b4f9c5f21495358320b7da39d123c4
TayaAmbrose <156131218+TayaAmbrose@users.noreply.github.com>
2025-03-21 17:59:04 -0700

    adding files for Disable_FW

---
+++ Commands/Disable_FW/disablefw.ps1
@@ -0,0 +1,5 @@
+netsh advfirewall set  currentprofile state off
+netsh advfirewall set domainprofile state off
+netsh advfirewall set privateprofile state off
+netsh advfirewall set publicprofile state off
+netsh advfirewall set  allprofiles state off
